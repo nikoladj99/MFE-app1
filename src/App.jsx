221221f2d093e164eb2171480e98bfc89120b6cb
nikoladj99 <nikoladj@elfak.rs>
2022-10-27 22:50:45 +0200

    dodat remotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Component1 from "./component1";
-//import Component2 from "app2/Component2";
+import Component2 from "app2/Component2";
 
 import "./index.scss";
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
     <Component1></Component1>
+    <Component2></Component2>
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
