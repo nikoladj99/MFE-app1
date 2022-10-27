0f942e710a740dadd6f388fae6fe18b8533f1b23
nikoladj99 <nikoladj@elfak.rs>
2022-10-27 16:14:32 +0200

    dev or prod change

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,12 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 const deps = require("./package.json").dependencies;
-module.exports = {
+module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:3000/",
+    publicPath:
+      argv.mode === "development"
+        ? "http://localhost:3000/"
+        : "https://app1-phi.vercel.app/",
   },
 
   resolve: {
@@ -65,4 +68,4 @@ module.exports = {
       template: "./src/index.html",
     }),
   ],
-};
+});
